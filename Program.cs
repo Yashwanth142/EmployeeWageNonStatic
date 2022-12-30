dcf4180e2f341a7b79468466433c0a47f7cd80b8
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-30 15:19:44 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageNonStatic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+        }
+    }
+}
