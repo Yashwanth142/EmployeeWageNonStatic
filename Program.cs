66ae42fdb3d939df44d0eb659424f2452b3d64fd
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-04 14:59:10 +0530

    Refactor the code

--- Program.cs
+++ Program.cs
@@ -5,9 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-
-            EmployeeWage obj= new EmployeeWage();
-            obj.Employeewage();
+            EmployeeWage Refactor = new EmployeeWage();
+            Refactor.Cal_Wage();
 
         }
     }
