5d7f08ca714506bb927fd4869e852462c9e08798
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-06 12:44:00 +0530

    Interface added

---
+++ InterfaceEmployeeWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageNonStatic
+{
+    public interface InterfaceEmployeeWage
+    {
+        public void addCompanies(string Company, int wagePerHour, int maxdays, int maxhours);
+
+        public void computeWage();
+    }
+}
