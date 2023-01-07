084fc3b2cc0b532a25045e7aa7ce68c8e36d4910
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-01-07 08:46:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,15 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            employeeWage obj = new employeeWage("deloitte", 20, 20, 80);
+            employeeWage obj = new employeeWage("adobe", 20, 10, 80);
             obj.Cal_Wage();
             Console.WriteLine(obj.toString());
 
-            employeeWage obj1 = new employeeWage("adobe", 25, 10, 90);
+            employeeWage obj1 = new employeeWage("Dell", 35, 20, 90);
             obj1.Cal_Wage();
             Console.WriteLine(obj1.toString());
-
-
         }
     }
 }
